f44b6f843f37ff26da8059961c9c40ba00c7101f
jammymalina <jakub.jantosik@gmail.com>
2017-12-14 08:50:18 +0100

    Finally getting somewhere

--- .d.ts
+++ .d.ts
@@ -2,3 +2,5 @@ declare module '*.xml' {
   const content: string;
   export default content;
 }
+
+declare var Blockly: any;
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,34 @@ import * as blocklyToolboxXml from './config/blockly/toolbox/basic.xml';
 
 window.addEventListener('load', (ev: Event) => {
   console.log(blocklyToolboxXml);
+
+  const options = {
+    comments: true,
+    collapse: true,
+    media: './src/scratch-blocks/media/',
+    oneBasedIndex: true,
+    readOnly: false,
+    //rtl: false,
+    scrollbars: true,
+    trashcan: true,
+    //toolbox: null,
+    //horizontalLayout: false,
+    //toolboxPosition: 'start',
+    zoom: {
+      controls: true,
+      wheel: true,
+      startScale: 1.0,
+      maxScale: 4,
+      minScale: 0.25,
+      scaleSpeed: 1.1
+    },
+    rtl: true,
+    toolbox: blocklyToolboxXml,
+    horizontalLayout: true,
+    toolboxPosition: 'start'
+  };
+
+  console.log(Blockly);
+
+  Blockly.inject('blocklyWorkspace', options);
 });
